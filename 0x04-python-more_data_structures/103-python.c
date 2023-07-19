e08500f3e46919b23b7ca84d278e1c0bf6a92532
gekwealor <gekwealor@gmail.com>
2023-07-19 06:29:00 -0700

    Python objects

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - A function that prints bytes information
+ *
+ * @p: PyObject *p
+ * Return: no return
+ */
+void print_python_bytes(PyObject *p)
+{
+	char *string;
+	long int size, g, limit;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)(p))->ob_size;
+	string = ((PyBytesObject *)p)->ob_sval;
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+
+	if (size >= 10)
+		limit = 10;
+	else
+		limit = size + 1;
+
+	printf("  first %ld bytes:", limit);
+
+	for (g = 0; g < limit; g++)
+		if (string[i] >= 0)
+			printf(" %02x", string[g]);
+		else
+			printf(" %02x", 256 + string[g]);
+
+	printf("\n");
+}
+
+/**
+ * print_python_list - A function that prints list information
+ *
+ * @p: PyObject *p
+ * Return: no return
+ */
+void print_python_list(PyObject *p)
+{
+	long int size, g;
+	PyListObject *list;
+	PyObject *obj;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (g = 0; g < size; g++)
+	{
+		obj = ((PyListObject *)p)->ob_item[g];
+		printf("Element %ld: %s\n", g, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
+	}
+}
