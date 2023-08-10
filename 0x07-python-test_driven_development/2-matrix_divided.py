35d97f64a5813466e9480ba43d043a2cf9dc6163
gekwealor <gekwealor@gmail.com>
2023-08-10 06:43:19 -0400

    Divided matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+"""
+  Module that divides a matrix
+"""
 
-matrix = [
-        [1, 2, 3],
-        [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
-print(matrix)
+
+def matrix_divided(matrix, div):
+    """
+      Function that divides a matrix of lists
+    """
+    if type(matrix) != list or not all(isinstance(i, list) for i in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) \
+                of integers/floats")
+    if not all(isinstance(j, (int, float)) for i in matrix for j in i):
+        raise TypeError("matrix must be a matrix (list of lists) \
+                of integers/floats")
+    if not all(len(i) == len(matrix[0]) for i in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(j / div, 2) for j in i] for i in matrix]
