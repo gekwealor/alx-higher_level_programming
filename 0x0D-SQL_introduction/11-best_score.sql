24acb2d9d04abe2fd6aa1cabbbd52fc0674120e5
gekwealor <gekwealor@gmail.com>
2023-09-19 08:46:50 -0400

    The best score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Show result of all record of the databases on the system.
 
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
