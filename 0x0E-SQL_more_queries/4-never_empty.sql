90a8b4398a82cf1a4f71c028af4b14d8ec1c7326
gekwealor <gekwealor@gmail.com>
2023-09-20 08:45:40 -0400

    Empty is a no

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
---- Script that creates a table and makes its ID value default of 1
+-- Script that creates a table and makes its ID value default of 1
 CREATE TABLE IF NOT EXISTS id_not_null(
 	id INT DEFAULT 1,
 	name VARCHAR(256)
