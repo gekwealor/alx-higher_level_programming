53f3fede51e2c1f6d15d9d835088bffb62b5615e
gekwealor <gekwealor@gmail.com>
2023-10-16 08:39:22 -0400

    JS addition

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
+const integers = require('process');
+const a = parseInt(integers.argv[2]);
+const b = parseInt(integers.argv[3]);
+
 function add (a, b) {
   console.log(a + b);
-
-if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
-  console.log('Arguments must be integers');
-} else {
-  const a = parseInt(process.argv[2]);
-  const b = parseInt(process.argv[3]);
-  add(a, b);
 }
+add(a, b);
