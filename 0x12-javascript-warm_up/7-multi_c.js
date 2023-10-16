0dd498b22b13e7925f6a7e2d7476f0fbb60f98d0
gekwealor <gekwealor@gmail.com>
2023-10-16 08:37:35 -0400

    C-multi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 
-const arg = process.argv.slice(2);
+const { argv } = require('process');
+const num = parseInt(argv[2]);
+const printC = (value) => {
+  for (; value > 0; value--) console.log('C is fun');
+};
 
-if (arg.length !== 1) {
-  console.log('Missing number of occurrences');
-} else {
-  const x = parseInt(arg[0]);
-  if (isNaN(x)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < x; i++) {
-      console.log('C is fun');
-    }
-  }
-}
+Number.isInteger(num) ? printC(num) : console.log('Missing numder of occurrence');
