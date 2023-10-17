ffafc4e2f8a923bd213b5ac75cf1f7812f6506bc
gekwealor <gekwealor@gmail.com>
2023-10-17 09:07:23 -0400

    Maps

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// script, that imports an array and computes a new array.
+
+const { list } = require('./100-data');
+console.log(list);
+console.log(list.map((element, idx) => element * idx));
