a7b596919456535ff1549de37240f6b1ed29782e
gekwealor <gekwealor@gmail.com>
2023-10-21 10:38:40 -0400

    Safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-
-"""
-  Script that takes an input from user
-  and makes a query to the database
-  but also safe from SQLinjection
-"""
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    """Takes an input but safely"""
-    db = MySQLdb.connect(
-            host='localhost', port=3306, usr=argv[1], pswd=argv[2],
-            db=argv[3])
 
-    cursor = db.cursor()
-
-    sql_query = "SELECT * FROM states WHERE name LIKE BINARY %s \
-    ORDER BY id ASC"
-
-    cursor.execute(sql_query, (argv[4] + '%',))
-
-    rows = cursor.fetchall()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
