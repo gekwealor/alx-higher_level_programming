fa1b9669225c1835f839ea78737bf874d0957c02
gekwealor <gekwealor@gmail.com>
2023-10-21 10:40:32 -0400

    Cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-
-"""
-  Script that takes a query and execute
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
-if __name__ == '__main__':
+import sys
 
-    db = MySQLdb.connect(
-            host='localhost', port=3306, usr=argv[1],
-            pswd=argv[1], db=argv[3])
-    cursor = db.cursor()
 
-    mysql_query = 'SELECT cities.id, cities.name, states.name FROM \
-    cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC'
-
-    cursor.execute(mysql_query)
-
-    rows = cursor.fetchall()
-    if rows is not None:
-        for row in rows:
-            print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
