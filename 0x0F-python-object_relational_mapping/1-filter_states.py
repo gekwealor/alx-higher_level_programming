4b801a295d012e14ecf9d197a8652ffea97d1935
gekwealor <gekwealor@gmail.com>
2023-10-21 10:35:11 -0400

    Fltered

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""Script that selects state with N"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    """
-    Connects to the database and makes a query
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    db = MySQLdb.connect(
-            host='localhost', port=3306, usr=username, pswd=password, db=dbs)
-
-    cursor = db.cursor()
-
-    cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY \
-                    'N%' ORDER BY states.id ASC")
-
-    rows = cursor.fetchall()
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
