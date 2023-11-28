13760734a40719f51f48da284e322de71d2556c6
gekwealor <gekwealor@gmail.com>
2023-11-28 05:13:11 -0800

    Status

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that takes a url and makes a get request
+// then prints the status
+
+const args = process.argv;
+
+const request = require('request');
+
+request(args[2], function (error, response, body) {
+  if (error) console.error(error);
+  console.log('code:', response.statusCode);
+});
